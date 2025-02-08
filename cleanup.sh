0d3ef12be66d8f1280fbf83e2d4120cffed22174
Niccolo' Zanotti <niccolo.zanotti@icloud.com>
2025-02-08 18:20:06 +0100

    fix(cleanup): increase depth to cleanup inner folder unwanted files

--- cleanup.sh
+++ cleanup.sh
@@ -6,8 +6,7 @@ echo "Cleaning up old auxiliary files..."
 # Remove matching files in the current directory
 rm -f *.{aux,bbl,bcf,blg,log,lot,out,toc,run.xml,synctex.gz,bak0,bak1,bak2,bak3,bak4,bak5,bak6,bak7,bak8,bak9,bak10}
 
-# Remove matching files in subdirectories up to a depth of 2
-find . -maxdepth 2 -type f \( \
+find . -maxdepth 3 -type f \( \
     -name "*.aux" -o \
     -name "*.bbl" -o \
     -name "*.bcf" -o \
